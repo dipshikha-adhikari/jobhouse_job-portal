9c5d8a9f91905ff8fc7e30aa40855b9eeb034c87
akbiv <akbivash@gmail.com>
2024-01-09 15:10:33 +0545

    Implement pagination

---
+++ client/src/components/shared/Pagination.tsx
@@ -0,0 +1,43 @@
+
+type Pagination = {
+    limit:number
+    totalLength:number
+    offset:number
+    setOffset:(props:number) => void
+}
+
+const Pagination = ({limit, totalLength,offset, setOffset}:Pagination) => {
+const index = offset/limit
+
+  const pages = Math.ceil(totalLength/limit)
+   const array = new Array(pages).fill(0).map((i,ind) => ind + 'x')
+
+ const handleNext = () => {
+    if(offset <  totalLength - limit){
+        setOffset(offset + limit)
+       }
+ }
+ const handlePrev = () => {
+    if(offset >= limit){
+        setOffset(offset - limit)
+       }
+ }
+ 
+const handlePageNumber = (ind:number) => {
+ setOffset(ind * limit)
+    }
+
+  return (
+    <div className="flex flex-wrap justify-end gap-xs p-md">
+         <button className="border-sm  text-green-dark cursor-pointer border-green-dark rounded-sm p-sm disabled:opacity-60 " disabled={offset < limit}  onClick={handlePrev}>Prev</button>
+    
+        {array.map((a,ind) => {
+            return <span key={a} className={`${index === ind && 'bg-green-dark text-white'} border-sm cursor-pointer border-gray-default rounded-sm p-sm `} onClick={() => handlePageNumber(ind)}>{ind + 1}</span>
+        })}
+         <button className="border-sm text-green-dark border-green-dark rounded-sm p-sm cursor-pointer disabled:opacity-60" disabled={offset >=  totalLength - limit} onClick={handleNext}>Next</button>
+       
+    </div>
+  )
+}
+
+export default Pagination
---
+++ client/src/pages/home/AllJobs.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { CiStar } from "react-icons/ci";
+import { useQuery } from "react-query";
+import JobCard from "../../components/shared/JobCard";
+import Loader from "../../components/shared/Loader";
+import Pagination from "../../components/shared/Pagination";
+import { publicRequest } from "../../lib/axios";
+import { AppliedJobs, IJob } from "../../types/postgres/types";
+import { useAppliedJobs } from "../jobseeker/hooks/useAppliedJobs";
+
+type AppliedJobsType = {
+  jobs: AppliedJobs[];
+  isLoading: boolean;
+  isError: boolean;
+};
+type Props = {
+  height?: number;
+};
+
+const AllJobs = ({ height }: Props) => {
+  const { jobs: appliedJobs }: AppliedJobsType = useAppliedJobs();
+  const [offset, setOffset] = useState(0);
+  const limit = 4;
+
+  useEffect(() => {
+    if (height) {
+      window.scrollTo(0, height);
+    }
+  }, [offset]);
+
+  const { data: allJobsCount } = useQuery(
+    ["allJobsCount", offset],
+    async () => {
+      const result = await publicRequest.get("/api/v1/jobs/count");
+      return result.data;
+    }
+  );
+
+  const getAllJobs = async () => {
+    const res = await publicRequest.get(
+      `/api/v1/jobs/?limit=${limit}&offset=${offset}`
+    );
+    return res.data;
+  };
+  const {
+    data: jobs,
+    isLoading,
+    isError,
+  } = useQuery<IJob[]>(["allJobs", offset], getAllJobs);
+
+  if (isLoading) return <Loader />;
+  if (isError) return <div className="text-center">Error!</div>;
+
+  return (
+    <div className=" border-sm ">
+      <header className="  border-b-sm flex font-bold items-center gap-2  p-sm   text-green-dark">
+        <CiStar className="text-green-dark " /> Top jobs
+      </header>
+      <div className="grid gap-xs p-sm place-items-center  grid-cols-auto-sm md:grid-cols-auto-md">
+        {jobs?.map((job) => {
+          return (
+            <JobCard appliedJobs={appliedJobs} job={job} key={job.job_id} />
+          );
+        })}
+      </div>
+      {allJobsCount?.count && (
+        <Pagination
+          offset={offset}
+          setOffset={setOffset}
+          totalLength={allJobsCount?.count}
+          limit={limit}
+        />
+      )}
+    </div>
+  );
+};
+
+export default AllJobs;
