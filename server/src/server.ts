23d9718eb4a48137fa4b8b8220f1d04e41b4af56
akbiv <akbivash@gmail.com>
2024-02-29 23:29:30 +0545

    Fix error

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,6 +13,10 @@ cloudinary.config({
   secure: true
 })
 
+app.get('/', (req: Request, res: Response) => {
+  return res.status(200).send({ msg: 'hello from server' })
+})
+
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cors({ origin: process.env.CLIENT_URL }))
@@ -20,8 +24,6 @@ app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
-app.get('/', (req: Request, res: Response) => {
-  return res.status(200).send({ msg: 'hello from server' })
-})
+
 
 app.listen('5000', () => console.log('server running'))
